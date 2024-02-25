708721849706e56d13ab513b828a6a651fa70a87
Raphaël BOICHOT <63796796+Raphael-Boichot@users.noreply.github.com>
2024-02-25 08:41:54 +0100

    Add files via upload

--- Codes/Run_Progressive_ESO_method.m
+++ Codes/Run_Progressive_ESO_method.m
@@ -21,7 +21,7 @@ p_vol=1e6;                      %surface of volume power
 filling_ratio=0.3;              %ratio of conductive matter on the surface
 starting_image='12x24.bmp';     %self explanatory
 %----------Hyper parameters for the ESO algorithm--------------------------
-max_rank=5;                     %maximum rank for exchange
+max_rank=2;                     %maximum allowed rank for exchange
 max_cell_swap=1;                %maximum number of simultaneous cell swap
 max_redounding_move_allowed=20; %stopping criterion, max_redounding_move_allowed*(1+step)
 max_steps=3;                    %number of scales tried into the run, 0 is the first run 3 is 2^3 scaling factor
@@ -82,7 +82,24 @@ end
 if flag==1
     disp('Filling blank image with conductive pixels...')
     number_conductive_cells=ceil(non_conductive_cells*filling_ratio);
-    boundary_conditions=init_image(boundary_conditions,number_conductive_cells, low_conductivity, high_conductivity);
+    %boundary_conditions=init_image(boundary_conditions,number_conductive_cells, low_conductivity, high_conductivity);
+    %***********************
+    tmax_ini=1e15;
+    disp('Trying some Monte Carlo search for beginning with a not too bad topology')
+    for i=1:20
+        temp_conditions=init_image(boundary_conditions,number_conductive_cells, low_conductivity, high_conductivity);
+        for j=1:1:20
+            [temp_conditions,~,~] = fun_ESO_algorithm(temp_conditions,high_conductivity,low_conductivity,heat_sink_temperature,delta_x,p_vol, max_rank, max_cell_swap);
+        end
+        [distance,somme_entropie, entropie, border_variance,variance, moyenne_temp,t_max,temp,grad,variance_grad]=finite_temp_direct_sparse(high_conductivity,low_conductivity,heat_sink_temperature,delta_x,p_vol,temp_conditions);
+        if t_max<tmax_ini
+            tmax_ini=t_max;
+            best_initial_topology=temp_conditions;
+            disp('Best topology found !')
+        end
+    end
+    boundary_conditions=best_initial_topology;
+    %***********************
 end
 
 disp('Starting the ESO algorithm...');
@@ -106,7 +123,7 @@ while not(step==(max_steps+1))
         [boundary_conditions,growth,etching] = fun_ESO_algorithm(boundary_conditions,high_conductivity,low_conductivity,heat_sink_temperature,delta_x,p_vol, max_rank, max_cell_swap);
         [distance,somme_entropie, entropie, border_variance,variance, moyenne_temp,t_max,temp,grad,variance_grad]=finite_temp_direct_sparse(high_conductivity,low_conductivity,heat_sink_temperature,delta_x,p_vol,boundary_conditions);
         history_tmax(m-last_valid_file)=t_max;
-
+        
         for k = 1:1:height
             for l = 1:1:width
                 pixel = boundary_conditions(k, l) ;
@@ -135,43 +152,43 @@ while not(step==(max_steps+1))
                 boundary_output(k,l,3)=blue;
             end
         end
-
+        
         boundary_output=uint8(boundary_output);
         mirror=fliplr(boundary_output(1:height,1:width-1,:));
         mirror2=fliplr(mirror);
         arbre=[mirror2,mirror];
         figure(1)
         subplot(2,4,1:2);
-
+        
         if (m-last_valid_file)>2
             if (m-last_valid_file)<100; plot(history_tmax,'.r'); end
             if (m-last_valid_file)>=100; plot((history_tmax(end-98:end)),'.r'); end
         end
         title('Max temperature');
-
+        
         subplot(2,4,3:4);
         imagesc([mirror2,mirror]);
         title('Topology');
-
+        
         subplot(2,4,5);
         plot(variance,'.m');
         imagesc(log10(entropie(2:end-1,2:end-1)));
         title('Log10 Entropy');
-
+        
         subplot(2,4,6);
         imagesc(temp);
         title('Temperature');
-
+        
         subplot(2,4,7);
         imagesc(grad);
         title('Gradients');
-
+        
         old_max_history=max(max(history_map));
         for i=1:1:max_cell_swap
             history_map(growth(i,1),growth(i,2))=history_map(growth(i,1),growth(i,2))+1;
             history_map(etching(i,1),etching(i,2))=history_map(etching(i,1),etching(i,2))+1;
         end
-
+        
         %allows to avoid cell swapping again and again at the same positions
         if max(max(history_map))>old_max_history
             max_cell_swap=max_cell_swap-1;
@@ -179,7 +196,7 @@ while not(step==(max_steps+1))
                 max_cell_swap=1;
             end
         end
-
+        
         %allows speed up convergence a bit
         %     if rand<0.01
         %         max_cell_swap=max_cell_swap+1;
@@ -187,22 +204,22 @@ while not(step==(max_steps+1))
         %             max_cell_swap=max_rank;
         %         end
         %     end
-
+        
         subplot(2,4,8);
         imagesc(sqrt(history_map));
         title('History map');
-
+        
         disp(['Maximal temperature: ',num2str(history_tmax(m-last_valid_file))]);
         initial_boundary_conditions=boundary_output;
         colormap jet
         drawnow;
-
+        
         if history_tmax(m-last_valid_file)<Max_temperature
             Max_temperature=history_tmax(m-last_valid_file);
             Best_topology=boundary_conditions;
             disp('*********Best topology detected !**********')
         end
-
+        
         saveas(gcf,['Figure_kp_ko_',num2str(high_conductivity),'_phi_',num2str(filling_ratio),'.png']);
         imwrite(arbre,['Topology_kp_ko_',num2str(high_conductivity),'_phi_',num2str(filling_ratio),'.png']);
         saveas(gcf,['Figure/Figure_kp_ko_',num2str(high_conductivity),'_phi_',num2str(filling_ratio),'_',num2str(m,'%06.f'),'.png']);
